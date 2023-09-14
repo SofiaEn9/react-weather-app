8f7ec84eb282270d8e31e0101ec7253a153f3316
SofiaEn9 <sofiaanen9@gmail.com>
2023-09-14 15:56:18 -0600

    Fetch correct data from API on Forecast Day1

--- src/DailyForecast.js
+++ src/DailyForecast.js
@@ -1,32 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import "./DailyForecast.css";
 import axios from "axios";
+import DailyForecastDay1 from "./DailyForecastDay1";
 
 export default function DailyForecast(props) {
+  let [loaded, setLoaded] = useState(false);
+  let [forecast, setForecast] = useState(null);
+
   function handleResponse(response) {
-    console.log(response.data);
+    setForecast(response.data.daily);
+    setLoaded(true);
   }
 
-  let apiKey = "de926abe0f35af91c8149b6305ofa34t";
-  let longitude = props.coordinates.longitude;
-  let latitude = props.coordinates.latitude;
-  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lat=${latitude}&lon=${longitude}&key=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(handleResponse);
-
-  return (
-    <div className="col DailyForecast">
-      <div className="row">
-        <div className="col">
-          <img src={props.icon} alt="{props.iconDescription}"></img>
-        </div>
-        <div className="col DailyForecast-specs">
-          <div className="DailyForecast-day">Day 1</div>
-          <div className="DailyForecast-temperature">
-            <span className="DailyForecast-temperature-max">32º</span>
-            <span className="DailyForecast-temperature-min"> | 21º</span>
-          </div>
-        </div>
+  if (loaded) {
+    return (
+      <div className="col DailyForecast">
+        <DailyForecastDay1 data={forecast[0]} />
       </div>
-    </div>
-  );
+    );
+  } else {
+    let apiKey = "de926abe0f35af91c8149b6305ofa34t";
+    let longitude = props.coordinates.longitude;
+    let latitude = props.coordinates.latitude;
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lat=${latitude}&lon=${longitude}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+
+    return null;
+  }
 }
---
+++ src/DailyForecastDay1.js
@@ -0,0 +1,49 @@
+import React from "react";
+import "./DailyForecast.css";
+
+export default function DailyForecastDay1(props) {
+  function maxTemperature() {
+    let temperature = Math.round(props.data.temperature.maximum);
+    return `${temperature}º`;
+  }
+  function minTemperature() {
+    let temperature = Math.round(props.data.temperature.minimum);
+    return `${temperature}º`;
+  }
+
+  function day() {
+    let date = new Date(props.data.time * 1000);
+    let day = date.getDay();
+
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tueday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Satday",
+    ];
+    return days[day];
+  }
+
+  return (
+    <div className="DailyForecastDay1">
+      <div className="row">
+        <div className="col">
+          <img
+            src={props.data.condition.icon_url}
+            alt={props.data.condition.description}
+          ></img>
+        </div>
+        <div className="col specs">
+          <div className="day">{day()}</div>
+          <div className="temperature">
+            <span className="temperature-max">{maxTemperature()}</span>
+            <span className="temperature-min"> | {minTemperature()}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
